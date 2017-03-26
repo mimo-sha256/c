babb0317a3beb622d28b7f23c9eda788268613d1
yamadoot <mihirmohanm4@gmail.com>
2017-03-26 11:43:24 +0530

    addition of n numbers

--- pointer.c
+++ pointer.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
-void fun(int *p);
+void get_add(int n,double *p,double *s);
 int main(){
-int i;
-int a[10];
-int *q;
-q=&a[0];
-fun(q);
-for(i=0;i<10;i++){
-printf("%d\t",*(q+i));
+	int n,i;
+	double sum;
+	printf("enter how many numbers you want to add:");
+	scanf("%d",&n);
+	double a[n];
+	double *q;
+	q=&sum;
+	get_add(n,&a[0],q);
+	printf("the sum is %lf\n",*q);
 }
-printf("\n");
-}
-void fun(int *p){
-int i;
-for(i=0;i<10;i++){
-*(p+i)=i+10;
-}
+
+void get_add(int n,double *p,double *s){
+	int i;
+	for(i=0;i<n;i++){
+		printf("enter %d number:",i+1);
+		scanf("%lf",(p+i));
+		*s+=*(p+i);
+	}
 }
